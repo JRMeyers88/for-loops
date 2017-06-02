98e3c6d435b8de471c6356a0f737469647d1db8b
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-06-02 11:53:00 -0500

    for loops

---
+++ for-loops.js
@@ -0,0 +1,37 @@
+
+
+for (var i = 5; i <= 120; i = i + 10) {
+	console.log(i);
+}
+
+for (var i = 4096; i >= 1; i = i/2) {
+	console.log(i);
+}
+
+
+
+var president = ["George Washington", "John Adams", "Thomas Jefferson"];
+	for (var i = 0; i < president.length; i++) {
+
+	console.log (`president ${i+1} was ${president[i]}`);
+
+}
+
+
+var antSpecies = {
+  argentine: {a},
+  army: {b},
+  bigHeaded: {c},
+  black: {d},
+  bull: {e},
+  carpenter: {f},
+  crazy: {g},
+  fire: {h},
+  glider: {i},
+  honeyPot: {j},
+  jackJumper: {k}
+}
+
+for (var prob in antSpecies) {
+	console.log(`.antSpecies${prop} = ${obj[prop]}`);
+}
